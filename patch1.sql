8de64eb43bac5149c64c9553ebd721d1d64e7e31
Atikur Rahaman Santo <55808925+ARSSANTO@users.noreply.github.com>
2022-11-25 11:14:47 +0600

    Update patch1.sql

--- patch1.sql
+++ patch1.sql
@@ -1,9 +1,15 @@
---- INSTRUCTIONS ON THE README ARE OUTDATED, ONLY FOR DEVS WHO KNOW WHAT THEY ARE DOING
+-- - INSTRUCTIONS ON THE README ARE OUTDATED, ONLY FOR DEVS WHO KNOW WHAT THEY ARE DOING
 
---RUN ONLY WHEN USING StoreParkinglotAccuratly 
--- ALTER TABLE `player_vehicles`
--- ADD COLUMN `parkingspot` VARCHAR(200) NULL DEFAULT NULL AFTER `garage`;
+-- RUN ONLY WHEN USING StoreParkinglotAccuratly 
+ALTER TABLE `player_vehicles`
+ADD COLUMN `parkingspot` VARCHAR(200) NULL DEFAULT NULL AFTER `garage`;
 
 -- RUN ONLY WHEN USING StoreDamageAccuratly
--- ALTER TABLE `player_vehicles`
--- ADD COLUMN `damage` VARCHAR(1500) NULL DEFAULT NULL AFTER `garage`;
+ALTER TABLE `player_vehicles`
+ADD COLUMN `damage` VARCHAR(1500) NULL DEFAULT NULL AFTER `garage`;
+
+--RUN THIS ONLY WHEN USING StoreParkinglotAccuratly & StoreDamageAccuratly
+
+ALTER TABLE `player_vehicles`
+ADD COLUMN `parkingspot` VARCHAR(150) NULL DEFAULT NULL AFTER `garage`,
+ADD COLUMN `damage` VARCHAR(1500) NULL DEFAULT NULL AFTER `parkingspot`;
